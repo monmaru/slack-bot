96c926bea6ac3062d968b4fb7034871b5ea99160
Yoshitomo Minami <minami530@gmail.com>
2016-09-21 21:15:52 +0900

    Initial commit

---
+++ bot.js
@@ -0,0 +1,44 @@
+var trending = require('github-trending');
+var Botkit = require('botkit');
+var CronJob = require('cron').CronJob;
+var controller = Botkit.slackbot();
+var bot = controller.spawn({
+  token: process.env.token
+}).startRTM(function(err, bot, payload) {
+  if (err) {
+    throw new Error('Could not connect to Slack');
+  }
+  
+  new CronJob({
+    cronTime: '00 00 19 * * 0-6',
+    onTick: function() {
+      trending('C#', function(err, repos) {
+        if (err) {
+          console.log(err);
+          throw new Error('Could not acces to Github');
+        }
+
+        var LF = '\n';
+        for(var i = 0; i < repos.length; i++) {
+          var message = '■' + repos[i].title + LF
+                      + repos[i].url + LF
+                      + repos[i].description + LF
+                      + repos[i].star + LF
+                      + '------------------------------------' + LF;
+          bot.say({
+            channel: 'general',
+            text: messageBody,
+            username: 'github_bot',
+            icon_url: ''
+          });
+        }
+      });
+    },
+    start: true,
+    timeZone: 'Asia/Tokyo'
+  });
+});
+
+controller.hears(["こんにちわ"],["direct_message","direct_mention","mention"], function(bot, message) {
+  bot.reply(message, 'こんにちわ');
+});

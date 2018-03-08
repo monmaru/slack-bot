df6973e463436e9b9e9d3582d5ace273669723d0
Yoshitomo Minami <minami530@gmail.com>
2018-03-08 22:50:17 +0900

    Added unit test

---
+++ test/github.spec.js
@@ -0,0 +1,20 @@
+import assert from 'power-assert';
+import Trend from '../src/github';
+
+describe('trend', () => {
+  describe('#fetch()', () => {
+    it('should be success', async function() {
+      this.timeout(20000);
+      const expected = 'string';
+      const trend = new Trend();
+      const repos = await trend.fetch('javascript');
+      repos.forEach((r) => {
+        assert(typeof r.title === expected);
+        assert(typeof r.url === expected);
+        assert(typeof r.description === expected);
+        assert(typeof r.language === expected);
+        assert(typeof r.star === expected);
+      });
+    });
+  });
+});

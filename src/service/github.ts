ffdee75fca81b0ed84b9c0c670bc02d374fe7321
Yoshitomo Minami <minami530@gmail.com>
2019-08-04 13:16:33 +0900

    Fix scraping on github

--- src/service/github.ts
+++ src/service/github.ts
@@ -14,17 +14,17 @@ export class TrendService {
   async fetch(lang: string): Promise<Trend[]> {
     const data = await getResponseData<string>(`https://github.com/trending/${encodeURIComponent(lang)}`);
     const $ = cheerio.load(data);
-    return Array.from($('.repo-list li')).map(element => {
+    return Array.from($('div.application-main div.Box article.Box-row')).map(element => {
       const $elem = $(element);
-      const url = $elem.find('h3 a').attr('href');
+      const url = $elem.find('h1 a').attr('href');
       const refs = url.split('/');
 
       return {
         owner: refs[1],
         title: refs[2],
-        url: 'https://github.com' + url,
+        url: `https://github.com${url}`,
         description: $elem
-          .find('div[class="py-1"] p')
+          .find('p.text-gray')
           .text()
           .trim(),
         language: $elem
@@ -32,7 +32,8 @@ export class TrendService {
           .text()
           .trim(),
         star: $elem
-          .find('span[class="float-right"]')
+          .find('a.muted-link')
+          .first()
           .text()
           .trim(),
       };
